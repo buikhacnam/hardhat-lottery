14c8f8ce4ad6ecf39a67fbf86d9b7e78251bac2f
Bui Nam <buikhacnam11@gmail.com>
2022-06-20 11:21:30 +0700

    Implementing Chainlink VRF to genarate random number

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,18 +1,49 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 import "hardhat/console.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 error Raffle__NotEnoughETHEnter();
+error Raffle_TransferFailed();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players; //payable here means that the address is a payable address (one of them will win and receive the prize)
 
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+
+    // see https://docs.chain.link/docs/vrf-contracts/#configurations
+    // bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit; //The limit for how much gas to use for the callback request to your contract's fulfillRandomWords() function. It must be less than the maxGasLimit limit on the coordinator contract.
+    uint32 private constant NUM_WORDS = 1;
+
+    address private s_recentWinner;
+
     event RaffleEnter(address indexed player);
-    
-    constructor(uint256 entranceFee) {
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
+
+    constructor(
+        // Rinkeby coordinator. For other networks,
+        // see https://docs.chain.link/docs/vrf-contracts/#configurations
+        // address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -25,12 +56,43 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
-    function getEntranceFee() public view returns(uint256) {
+    function requestRandomWinner() external {
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
+    }
+
+    function fulfillRandomWords(
+        uint256, /*requestId*/
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle_TransferFailed();
+        }
+
+        //PROBLEM: need a way to keep track of the list of winners
+        //SOLUTION: emit an event here so we can later query  history of winner picked events
+        emit WinnerPicked(recentWinner);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 
-    function getPlayer(uint256 index) public view returns(address) {
+    function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
     }
-    
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
+    }
 }
